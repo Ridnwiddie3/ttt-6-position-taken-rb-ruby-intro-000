0c334b9af4a71f1ddbf8fd2222d1f787dec431ad
Richard Dinwiddie <rdinwiddie3@gmail.com>
2018-09-19 16:56:40 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,12 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
-array[index_number]
-
+if board[index] == " "
+  false
+elsif board[index] == ""
+  false
+elsif board[position] == nil
+  false
+else board[position] == "X" || "O"
+  true
+  
 end
